7d26dececa8ac021e8d3813a76cad386e997df39
Lu1s-15 <152184941+Lu1s-15@users.noreply.github.com>
2023-12-04 16:38:48 +0100

    try3

--- weather000.py
+++ weather000.py
@@ -26,60 +26,6 @@ def get_weather_forecast(city):
 
     if response.status_code == 200:
         weather_data = response.text
-        display_weather_forecast(weather_data)
+        print(weather_data)
     else:
         print("Failed to fetch weather forecast")
-
-def display_weather_forecast(weather_data):
-    result_window = tk.Toplevel(root)
-    result_window.title("Weather Forecast")
-
-    result_frame = tk.Frame(result_window)
-    result_frame.pack(fill='both', expand=True)
-
-    canvas = tk.Canvas(result_frame)
-    canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-    scrollbar = tk.Scrollbar(result_frame, command=canvas.yview)
-    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-    canvas.configure(yscrollcommand=scrollbar.set)
-
-    frame = tk.Frame(canvas)
-    canvas.create_window((0, 0), window=frame, anchor='nw')
-
-    # Use CSV reader to parse the response data
-    reader = csv.DictReader(StringIO(weather_data))
-
-    # Frame for each day's data with a border
-    for row in reader:
-        day_frame = tk.Frame(frame, relief=tk.RIDGE, borderwidth=2)
-        day_frame.pack(pady=5, padx=10, anchor='w')
-
-        for key, value in row.items():
-            param_label = tk.Label(day_frame, text=f"{key}: {value}", font=("Arial", 11))
-            param_label.pack(anchor='w')
-
-    def on_configure(event):
-        canvas.configure(scrollregion=canvas.bbox("all"), width=500, height=400)
-
-    canvas.bind('<Configure>', on_configure)
-
-def on_submit():
-    city = city_entry.get()
-    get_weather_forecast(city)
-
-root = tk.Tk()
-root.title("Weather Forecast")
-root.geometry("600x500")  # Set window size
-
-label = tk.Label(root, text="Enter City:", font=("Arial", 14))
-label.pack(pady=10)
-
-city_entry = tk.Entry(root, font=("Arial", 12))
-city_entry.pack(pady=5)
-
-submit_button = tk.Button(root, text="Get Forecast", command=on_submit, font=("Arial", 12))
-submit_button.pack(pady=10)
-
-root.mainloop()
